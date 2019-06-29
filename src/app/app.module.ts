43c9bda868de82f523c812c37678944e2efdeab2
Henry Minh <henryminh@macbookpro.lan>
2019-06-29 06:55:47 -0700

    Update Routes for Dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,11 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './nav/nav.component';
 import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatGridListModule, MatCardModule, MatMenuModule } from '@angular/material';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule } from '@angular/material';
+import { MatIconModule, MatListModule, MatGridListModule } from '@angular/material';
+import { MatCardModule, MatMenuModule } from '@angular/material';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
