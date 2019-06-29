7ccc72c80beb8768fc03e5bc6c90e876d4790639
Henry Minh <henryminh@macbookpro.lan>
2019-06-29 07:28:54 -0700

    material:material-table customer-list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,17 +8,19 @@ import { NavComponent } from './nav/nav.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule, MatButtonModule, MatSidenavModule } from '@angular/material';
 import { MatIconModule, MatListModule, MatGridListModule } from '@angular/material';
-import { MatCardModule, MatMenuModule } from '@angular/material';
+import { MatCardModule, MatMenuModule, MatTableModule, MatPaginatorModule, MatSortModule } from '@angular/material';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { from } from 'rxjs';
 import { CustomerComponent } from './customer/customer.component';
+import { CustomerListComponent } from './customer/customer-list/customer-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     DashboardComponent,
-    CustomerComponent
+    CustomerComponent,
+    CustomerListComponent
   ],
   imports: [
     BrowserModule,
@@ -32,7 +34,10 @@ import { CustomerComponent } from './customer/customer.component';
     MatListModule,
     MatGridListModule,
     MatCardModule,
-    MatMenuModule
+    MatMenuModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule
   ],
   providers: [],
   bootstrap: [AppComponent]
